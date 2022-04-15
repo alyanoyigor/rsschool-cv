b19589570b18123e41a64b06db0b0d0980c9410a
Igor Alyanoy <igoralyanoy@gmail.com>
2022-04-15 18:05:38 +0300

    updated cv

--- js/app.js
+++ js/app.js
@@ -1,27 +1,19 @@
-const swiper = new Swiper(".swiper-container", {
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: "auto",
-	coverflowEffect: {
-		rotate: 50,
-		stretch: 0,
-		depth: 100,
-		modifier: 1,
-		slideShadows: true,
-	},
-	pagination: {
-		el: ".swiper-pagination",
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
-document.querySelector(".menu__btn").addEventListener("click", function () {
-	document.querySelector(".menu__list").classList.toggle("active");
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', function () {
+  menuList.classList.toggle('active');
 });
-
-hljs.highlightAll();
-
